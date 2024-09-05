1983b933c9175dea194fba06d685684cfc875967
Elango Palanisamy <elango@Elangos-Mac-mini.local>
2024-09-05 23:24:52 +0530

    final unsubscribe

--- app/(web)/unsubscribe/page.js
+++ app/(web)/unsubscribe/page.js
@@ -3,32 +3,44 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, Suspense } from "react";
 
-const Unsubscribe = () => {
+const UnsubscribeContent = () => {
   const params = useSearchParams();
   const email = params.get("email");
 
   useEffect(() => {
-    try {
-      fetch(`https://hooks.zapier.com/hooks/catch/11976044/2t02mgc/`, {
-        mode: "no-cors",
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          domain: window.location.hostname,
-        }),
-      });
-    } catch (error) {}
-  }, []);
+    if (email) {
+      try {
+        fetch(`https://hooks.zapier.com/hooks/catch/11976044/2t02mgc/`, {
+          mode: "no-cors",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            domain: window.location.hostname,
+          }),
+        });
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  }, [email]);
+
+  return (
+    <h1 className="text-3xl text-nl_background w-full md:w-1/2">
+      {email
+        ? `Your email (${email}) has been successfully removed from our subscriber list.`
+        : "Email not found."}
+    </h1>
+  );
+};
+
+const Unsubscribe = () => {
   return (
     <div className="bg-nl_sec_background min-h-svh md:min-h-[1050px] flex justify-center items-center px-4 md:px-8">
       <Suspense fallback={<div>Loading...</div>}>
-        <h1 className="text-3xl text-nl_background w-full md:w-1/2">
-          Your email({email}) has been successfully removed from our subscriber
-          list.
-        </h1>
+        <UnsubscribeContent />
       </Suspense>
     </div>
   );
