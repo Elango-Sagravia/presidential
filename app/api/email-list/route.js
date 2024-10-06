d108f63e6e21ae89eeaeb02ca1317a04456605d8
Elango-Sagravia <elango@sagravia.com>
2024-10-07 05:23:08 +0530

    removed NextResponse

--- app/api/email-list/route.js
+++ app/api/email-list/route.js
@@ -1,15 +1,12 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 import { query } from "@/lib/db";
 
 export async function GET() {
   try {
     const result = await query("SELECT * FROM users LIMIT 10;", []);
-    return NextResponse.json(result.rows);
+    return Response.json(result.rows);
   } catch (err) {
     console.error(err);
-    return NextResponse.json(
-      { message: "Error fetching users" },
-      { status: 500 }
-    );
+    return Response.json({ message: "Error fetching users" }, { status: 500 });
   }
 }
