b1bf7c127aa770ed8ba9e4dbd1b3d6d71e46a712
Elango-Sagravia <elango@sagravia.com>
2024-10-07 05:52:58 +0530

    added limit parameter

--- app/api/email-list/route.js
+++ app/api/email-list/route.js
@@ -1,9 +1,27 @@
 // import { NextResponse } from "next/server";
 import { query } from "@/lib/db";
 
-export async function GET() {
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const limit = searchParams.get("limit") || 0;
   try {
-    const result = await query("SELECT * FROM users LIMIT 10;", []);
+    const result = await query(
+      `(
+  SELECT *
+  FROM users
+  WHERE source_id = 1
+  LIMIT ${limit}
+)
+UNION
+(
+  SELECT *
+  FROM users
+  WHERE source_id = 2
+  LIMIT GREATEST(${limit} - (SELECT COUNT(*) FROM users WHERE source_id = 1), 0)
+)
+LIMIT ${limit};`,
+      []
+    );
     return Response.json(result.rows);
   } catch (err) {
     console.error(err);
