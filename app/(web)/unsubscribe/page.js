9a9e4e4d5ad01fff1f74dec7038ee3bd6741146c
Elango-Sagravia <elango@sagravia.com>
2024-10-07 14:25:30 +0530

    updated the component

--- app/(web)/unsubscribe/page.js
+++ app/(web)/unsubscribe/page.js
@@ -6,9 +6,27 @@ import { useEffect, Suspense } from "react";
 const UnsubscribeContent = () => {
   const params = useSearchParams();
   const email = params.get("email");
+  const userId = params.get("user_id"); // Assuming user_id is also part of the query parameters
+  const campaignId = params.get("campaign_id"); // Assuming campaign_id is part of the query parameters
 
   useEffect(() => {
-    if (email) {
+    if (email && userId && campaignId) {
+      // Call the unsubscribe GET function
+      const unsubscribeUrl = `https://${window.location.hostname}/api/emails-unsubscribe?user_id=${userId}&campaign_id=${campaignId}&email=${email}`;
+      try {
+        fetch(unsubscribeUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Unsubscribe result:", data);
+          })
+          .catch((error) => {
+            console.error("Error during unsubscribe:", error);
+          });
+      } catch (error) {
+        console.error("Error:", error);
+      }
+
+      // Call the Zapier webhook
       try {
         fetch(`https://hooks.zapier.com/hooks/catch/11976044/2t02mgc/`, {
           mode: "no-cors",
@@ -25,7 +43,7 @@ const UnsubscribeContent = () => {
         console.error("Error:", error);
       }
     }
-  }, [email]);
+  }, [email, userId, campaignId]);
 
   return (
     <h1 className="text-3xl text-nl_background w-full md:w-1/2">
