744b39e285906b840c26099aaaf3915a5122d16a
Elango-Sagravia <elango@sagravia.com>
2024-12-12 16:41:52 +0530

    added g tag

--- app/(web)/layout.js
+++ app/(web)/layout.js
@@ -52,17 +52,18 @@ export default function RootLayout({ children }) {
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
       />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+      <head>
+        <RedditPixel />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-P3XXTFG6');`,
-        }}
-      />
-      <head>
-        <RedditPixel />
+          }}
+        />
       </head>
       <body>
         <noscript>
