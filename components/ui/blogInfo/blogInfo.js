ef29602a67056729574ab2177669981dfc5be05f
Elango-Sagravia <elango@sagravia.com>
2025-01-17 18:10:55 +0530

    added new ad image

--- components/ui/blogInfo/blogInfo.js
+++ components/ui/blogInfo/blogInfo.js
@@ -1,9 +1,11 @@
-import { changeFormatDay } from "@/components/dateFormat";
+import { changeFormatDate } from "@/components/dateFormat";
 
 export default function BlogInfo({ date, read_time }) {
   return (
     <div className="flex items-center gap-2 py-4">
-      <p className="text-[12px] font-bold uppercase">{changeFormatDay(date)}</p>
+      <p className="text-[12px] font-bold uppercase">
+        {changeFormatDate(date)}
+      </p>
       <p className="rounded-full bg-black w-[5px] h-[5px]"></p>
       <p className="text-[12px] font-bold uppercase">{read_time} MIN READ</p>
     </div>
