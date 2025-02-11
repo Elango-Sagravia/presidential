7721bd9f163f2ef2e2e3cafbf4cd3d5f0473663f
Elangodevguy <elangodevguy@gmail.com>
2025-02-11 15:15:46 +0530

    added images for email

--- app/(web)/layout.js
+++ app/(web)/layout.js
@@ -92,6 +92,36 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6840958913821863"
           crossOrigin="anonymous"
         />
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "6948988";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            (function(l) {
+                if (!l) {
+                    window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                    window.lintrk.q = [];
+                }
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";
+                b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=6948988&fmt=gif"
+          />
+        </noscript>
       </head>
       <body>
         <ThemeProvider
