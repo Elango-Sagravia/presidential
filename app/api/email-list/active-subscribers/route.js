922ca424408ca7fc5b873f213decd33712fb9a2a
Elango-Sagravia <elango@sagravia.com>
2025-02-19 16:24:52 +0530

    fixed cache issue

--- app/api/email-list/active-subscribers/route.js
+++ app/api/email-list/active-subscribers/route.js
@@ -1,3 +1,6 @@
+export const runtime = "nodejs"; // ✅ Fix for Next.js build error
+export const dynamic = "force-dynamic";
+
 import { query } from "@/lib/db";
 
 export async function GET(request) {
