783cceecba0e2d3d830e0c87b8aa37b432241407
Elango-Sagravia <elango@sagravia.com>
2025-02-19 18:49:50 +0530

    added clarity

--- app/(web)/layout.js
+++ app/(web)/layout.js
@@ -92,6 +92,16 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6840958913821863"
           crossOrigin="anonymous"
         />
+        {/* Microsoft Clarity Script */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "qca9x88liz");
+    `}
+        </Script>
         {/* LinkedIn Insight Tag */}
         <Script id="linkedin-insight" strategy="afterInteractive">
           {`
