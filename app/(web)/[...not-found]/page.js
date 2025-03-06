35d95188cf404102049c197832325cfe63c98ea2
Elango-Sagravia <elango@sagravia.com>
2025-03-06 16:01:34 +0530

    added homepage redirection for 404 pages

--- app/(web)/[...not-found]/page.js
+++ app/(web)/[...not-found]/page.js
@@ -1,5 +1,25 @@
-import { notFound } from "next/navigation";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default function CatchAllNotFound() {
-  notFound(); // This will trigger `not-found.js` inside (web)
+export default function CatchAllRedirect() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000); // Redirects after 3 seconds
+  }, []);
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-nl_sec_background">
+      <div className="text-center">
+        <h1 className="text-2xl">404 - Page Not Found</h1>
+        <p className="mt-4">
+          Sorry, the page you are looking for does not exist.
+        </p>
+        <p className="mt-4">Redirecting to homepage....</p>
+      </div>
+    </div>
+  );
 }
