f320fa35c2bd0dad939deda0bf4cc97eb0ef7261
Elango-Sagravia <elango@sagravia.com>
2025-03-07 12:10:09 +0530

    replaced welcome email with email domain

--- app/api/emails/welcome/route.js
+++ app/api/emails/welcome/route.js
@@ -21,7 +21,7 @@ let transporter = nodemailer.createTransport({
 // Function to send an email
 async function sendEmail(email, slug, uniqueId) {
   let mailOptions = {
-    from: '"Presidential Summary" <no-reply@newsletter.presidentialsummary.com>', // Sender email address
+    from: '"Presidential Summary" <no-reply@email.presidentialsummary.com>', // Sender email address
     to: email, // Recipient email
     bcc: "elango@sagravia.com", // Add BCC recipient
     subject: "Action required: confirm your subscription",
