1e8f6e9babc219f85f43b3a09cf003455a23d574
Elango-Sagravia <elango@sagravia.com>
2025-05-05 18:53:29 +0530

    fixed sitemap changes

--- app/sitemap_articles/route.js
+++ app/sitemap_articles/route.js
@@ -1,10 +1,16 @@
-import { getDocumentSlugs } from "outstatic/server";
+import { getDocuments } from "outstatic/server";
 
 export const dynamic = "force-dynamic";
 
+async function getData() {
+  const articles = getDocuments("articles", ["slug"]);
+
+  return articles;
+}
+
 export async function GET() {
   const baseUrl = process.env.url;
-  const slugs = await getDocumentSlugs("articles");
+  const slugs = await getData();
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -12,7 +18,7 @@ ${slugs
   .map(
     (slug) => `
   <url>
-    <loc>${baseUrl}/articles/${slug}</loc>
+    <loc>${baseUrl}/articles/${slug.slug}</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
