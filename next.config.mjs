33efcd06a8a83eb0f8d47ac5d7b5b9cf6b7e8d2d
Elango-Sagravia <elango@sagravia.com>
2025-05-13 00:14:56 +0530

    revert back sitemap changes

--- next.config.mjs
+++ next.config.mjs
@@ -31,16 +31,16 @@ const nextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      { source: "/sitemap.xml", destination: "/sitemap" },
-      { source: "/sitemap_main.xml", destination: "/sitemap_main" },
-      { source: "/sitemap_blogs.xml", destination: "/sitemap_blogs" },
-      { source: "/sitemap_policies.xml", destination: "/sitemap_policies" },
-      { source: "/sitemap_pages.xml", destination: "/sitemap_pages" },
-      { source: "/sitemap_archives.xml", destination: "/sitemap_archives" },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     { source: "/sitemap.xml", destination: "/sitemap" },
+  //     { source: "/sitemap_main.xml", destination: "/sitemap_main" },
+  //     { source: "/sitemap_blogs.xml", destination: "/sitemap_blogs" },
+  //     { source: "/sitemap_policies.xml", destination: "/sitemap_policies" },
+  //     { source: "/sitemap_pages.xml", destination: "/sitemap_pages" },
+  //     { source: "/sitemap_archives.xml", destination: "/sitemap_archives" },
+  //   ];
+  // },
 };
 
 export default nextConfig;
