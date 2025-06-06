56540906f26830ccf2143e94a29fa92597059b32
Elango-Sagravia <elango@sagravia.com>
2025-06-06 15:45:00 +0530

    fixed 404

--- app/(web)/amp/archives/[slug]/generateStaticParams.js
+++
@@ -1,6 +0,0 @@
-import { getDocumentSlugs } from "outstatic/server";
-
-export async function generateStaticParams() {
-  const slugs = getDocumentSlugs("blogs");
-  return slugs.map((slug) => ({ slug }));
-}
